42d61ea3b6655d9b4dae07777c2566bbef76ac60
Francois Greeff <francois.greeff@bglgroup.co.uk>
2018-08-15 18:19:23 +0100

    Add argument for note

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const _ = require('lodash')
 
 const notes = require('./notes.js')
 
-var res = notes.addNote(`lovely note`)
+var command = process.argv[2]
+console.log(command)
+
+if (command === 'add') console.log('Adding new note')
+else if (command === 'list') console.log('Listing all notes')
+else if (command === 'read') console.log('Reading note')
+else if (command === 'remove') console.log('Removing note')
+else console.log('Command not recognised')
+
+/*var res = notes.addNote(`lovely note`)
 console.log(_.isString(res))
 
 var filteredArray = _.uniq(['this', 'more', 'that', 'and', 'this', 1, 1])
 console.log(filteredArray)
+*/
